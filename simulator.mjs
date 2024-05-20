7f1e53eaad6afba21e8688d30098d247d941315a
godotg <jaysunxiao@gmail.com>
2024-05-20 11:35:17 +0800

    doc[doc]: update doc

--- simulator.mjs
+++ simulator.mjs
@@ -1,6 +1,6 @@
 import clipboardy from 'clipboardy';
 import TurndownService from 'turndown';
-import {send, asyncAsk} from './websocket.mjs';
+import {asyncAsk, send} from './websocket.mjs';
 import ClipboardLockAsk from "./zfooes/packet/ClipboardLockAsk.mjs";
 import ClipboardUnlockAsk from "./zfooes/packet/ClipboardUnlockAsk.mjs";
 import SimulatorStatusAsk from "./zfooes/packet/SimulatorStatusAsk.mjs";
@@ -44,7 +44,7 @@ export function htmlToMarkdown(html) {
 // ---------------------------------------------------------------------------------------------------------------------
 export function sendNotLoginStatus(simulator) {
     const ask = new SimulatorStatusAsk();
-    ask.message = `${simulator} 没有登录，请您在AI模拟器打开的浏览器中登录，或者如果不想使用这个ai请在ai-config.yaml中移除改配置`;
+    ask.message = `${simulator} 没有登录，请您在AI模拟器打开的浏览器中登录（全部登录完成后，最好重启程序，避免浏览器无法对焦），或者如果不想使用这个ai请在ai-config.yaml中移除改配置`;
     send(ask);
 }
 
